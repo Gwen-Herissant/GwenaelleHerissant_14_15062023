8b11b7a9c11a932acd3edd87b940b363bc4a6ee1
Gwenaëlle Herissant <gh@pointligneplan.fr>
2023-07-06 15:35:03 +0200

    Navigation with react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import EmployeeList from './pages/EmployeeList';
+
+/**
+ * Generates routes for react-router
+ * @returns site's routes
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Homepage />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/employee-list" element={<EmployeeList />} />
+      </Routes>
+    </Router>
   );
 }
 
